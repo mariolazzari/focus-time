fa5cda26ff107972dd3015e096bed77b116f177d
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-15 23:41:24 +0200

    Done lesson 30

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    padding: 10,
     backgroundColor: "#252250",
   },
 });
--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
 
 const Focus = () => {
   return (
     <View style={styles.container}>
-      <Text>What you like to focus on</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What you like to focus on</Text>
+        <View style={styles.inputContainer}>
+          <TextInput />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +20,18 @@ export default Focus;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+  },
+  titleContainer: {
+    flex: 0.5,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    paddingTop: 20,
   },
 });
